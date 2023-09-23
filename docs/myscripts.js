fcac0592f82c7f36f2743e11124d7050a1cb0ab3
Andres Soto <u202116113@upc.edu.pe>
2023-09-23 10:10:36 -0500

    feat: added espanish html index and functional language menu

--- docs/myscripts.js
+++ docs/myscripts.js
@@ -18,49 +18,13 @@ function toggleLanguageMenu() {
 }
 
 
-function changeLanguage(lang, learn, benefits, about, plans, contact, log, create) {
-    var languageSelector = document.querySelector('.language-selector');
-    var languageIcon = languageSelector.querySelector('.fa');
-    var selectLearn = document.querySelector('.learn');
-    var selectBenefits = document.querySelector('.benefits');
-    var selectAbout = document.querySelector('.about-us');
-    var selectPlans = document.querySelector('.plans');
-    var selectContact = document.querySelector('.contact');
-    var selectLog = document.querySelector('.log-in');
-    var selectCreate =document.querySelector('.button-secondary');
-    languageSelector.innerHTML = lang + ' ' + languageIcon.outerHTML;
-    selectLearn.innerHTML = learn;
-    selectBenefits.innerHTML = benefits;
-    selectAbout.innerHTML = about;
-    selectPlans.innerHTML = plans;
-    selectContact.innerHTML = contact;
-    selectLog.innerHTML = log;
-    selectCreate.innerHTML = create;
-    toggleLanguageMenu();
-}
-
-
 document.addEventListener('click', function(event) {
-    var esLearn = 'Conoce más';
-    var enLearn = 'Learn';
-    var esBenefits = 'Beneficios';
-    var enBenefits = 'Benefits';
-    var esAbout = 'Acerca de';
-    var enAbout = 'About us';
-    var esPlans = 'Planes';
-    var enPlans = 'Plans';
-    var esContact = 'Contacto';
-    var enContact = 'Contact';
-    var esLog = 'Iniciar sesión';
-    var enLog = 'Log in';
-    var esCreate = 'Crear cuenta';
-    var enCreate = 'Create account';
     if (event.target.classList.contains('language-option')) {
         var selectedLanguage = event.target.textContent;
         if (selectedLanguage == 'EN'){
-            changeLanguage(selectedLanguage, enLearn, enBenefits, enAbout, enPlans, enContact, enLog, enCreate);
+            location.href="index.html";
         } else if (selectedLanguage == 'ES'){
-            changeLanguage(selectedLanguage, esLearn, esBenefits, esAbout, esPlans, esContact, esLog, esCreate);
+            location.href="es_index.html";
         }
     } 
 });
